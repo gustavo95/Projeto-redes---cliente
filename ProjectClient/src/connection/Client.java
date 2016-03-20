372585043dfd23f88d3696e4b94926329b74eb80
Gustavo Costa Gomes de Melo <gustavo_costa_gomes@hotmail.com>
2016-03-19 23:08:28 -0300

    first commit

---
+++ ProjectClient/src/connection/Client.java
@@ -0,0 +1,40 @@
+package connection;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+	public static void main(String argv[]) throws Exception {
+		String sentence;
+		String modifiedSentence;
+		System.out.println("CLIENTE INICIADO, DIGITE UM TEXTO: ");
+
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(
+				System.in));
+		
+		try{
+			Socket clientSocket = new Socket("localhost", 6789);
+
+			DataOutputStream outToServer = new DataOutputStream(
+					clientSocket.getOutputStream());
+
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+					clientSocket.getInputStream()));
+
+			sentence = inFromUser.readLine();
+
+			outToServer.writeBytes(sentence + '\n');
+
+			modifiedSentence = inFromServer.readLine();
+
+			System.out.println("FROM SERVER: " + modifiedSentence);
+
+			clientSocket.close();
+		}catch(Exception e){
+			System.out.println("Erro de conexão");
+		}
+
+	}
+}
